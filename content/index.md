87728fd00a777d5a36dfa75acf16b1b9a54ecf2f
Gatien Chenu <gatien+dev@chenu.me>
2026-03-18 02:13:34 +0100

    init

---
+++ content/index.md
@@ -0,0 +1,5 @@
+---
+title: Gatien Chenu, Computer vision for environmental monitoring
+---
+
+Hello I am Gatien CHENU,
--- quartz.config.ts
+++ quartz.config.ts
@@ -15,8 +15,8 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "fr-FR",
+    baseUrl: "gatienc.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
