41cb7a5694a52d816171b69a0020fde10fd397fc
Gatien Chenu <gatien+dev@chenu.me>
2026-03-18 02:39:32 +0100

    Quartz sync: Mar 18, 2026, 2:39 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "fr-FR",
+    locale: "en-US",
     baseUrl: "gatienc.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
